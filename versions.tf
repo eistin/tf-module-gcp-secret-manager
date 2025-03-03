5b57e2cd258dd79bb85dd18ba6c23f5454282d03
Istin <istin.edwin@gmail.com>
2025-03-03 03:45:53 +0100

    feat: upgrade GCP providers versions

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.9.0"
+  required_version = ">= 1.10.5"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.4.0"
+      version = "~> 6.23.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.4.0"
+      version = "~> 6.23.0"
     }
   }
 }
